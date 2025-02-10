30b1b75ac4e4cabd4bdd6d323d44e474c6ea6125
synnfall <synnfall@gmail.com>
2025-02-10 19:56:26 +0100

    begin of parcousup

--- mariage_complexe.c
+++ mariage_complexe.c
@@ -1,28 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
 
-typedef struct noeuds noeuds;
-struct noeuds 
+typedef struct _eleves eleves;
+typedef struct _formation formation;
+
+struct _formation
 {
     char nom[50];
 
-    noeuds* voisin;
-    int nb_voisin;
-    int capacite;
+    eleves** places;
+    int nb_places;
 
-    noeuds* liste_pref;
-    int len_liste_pref;
+    eleves** prefs;
+    int nb_eleves;
 };
 
-typedef struct 
+struct _eleves
 {
-    noeuds* grp1;
-    int len_grp1;
-    noeuds* grp2;
-    int len_grp2;
-} liste_noeuds;
+    char nom[50];
 
-int main()
+    formation* etablissement;
+
+    formation** voeux;
+    int nb_voeux;
+};
+
+
+uint32_t rand(int inf, int sup) {
+    static uint32_t seed = 0;
+
+    if (seed == 0) {
+        seed = time(NULL);
+    }
+
+    seed ^= seed << 13;
+    seed ^= seed >> 17;
+    seed ^= seed << 5;
+
+    return seed%(sup+1) + inf;
+}
+
+void shuffle(int *array, int size) {
+    for (int i = size - 1; i > 0; i--) 
+    {
+        int j = rand(0, size-1);
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+int formations_complete(formation** lst_formations, int nb_forms){
+    int complete = 1;
+    int i = 0;
+    while (i <nb_forms && complete)
+    {
+        formation* current_form = lst_formations[i];
+        if(current_form->nb_eleves<current_form->nb_places)
+        {
+            complete = 0;
+        }
+        i++;
+
+    }
+    return complete;
+}
+
+int tout_les_etudiant_ont_formation(eleves** lst_elvs, int nb_elvs){
+    int complete = 1;
+    int i = 0;
+    while (i <nb_elvs && complete)
+    {
+        eleves* current_elv = lst_elvs[i];
+        if(! current_elv->etablissement)
+        {
+            complete = 0;
+        }
+        i++;
+
+    }
+    return complete;
+}
+
+void attribue_formation(eleves** lst_elvs, int nb_elvs, formation** lst_formations, int nb_forms)
 {
-
+    while( !formations_complete(lst_formations,nb_forms) && ! tout_les_etudiant_ont_formation(lst_elvs, nb_elvs))
+    {
+        
+    }
+    return;
+}
+int main() {
+    return 0;
 }
